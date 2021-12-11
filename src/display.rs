306ad9a981459b2d9f4aeb1590ba843335a41543
mexus <null@mexus.xyz>
2021-12-11 18:42:16 +0300

    Frunk-free display

--- src/display.rs
+++ src/display.rs
@@ -1,73 +1,28 @@
 use std::fmt;
 
-use frunk::{HCons, HNil};
+use crate::{HeterogenousList, ListTerminator};
 
-/// A helper trait to trick the orphans rule which basically mimics the
-/// [fmt::Display].
+/// ```rust
+/// use playing_with_hlists::hlist;
 ///
-/// While it is technically possible to implement the trait, there is no sense
-/// in doing so: simply go with the [fmt::Display] instead.
-#[doc(hidden)]
-pub trait MyDisplay {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result;
-}
-
-impl<T: ?Sized + MyDisplay> MyDisplay for &'_ T {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        <T as MyDisplay>::fmt(self, f)
-    }
-}
-
-/// A wrapper that implements [fmt::Display]. Used for providing a
-/// [fmt::Display] implementation for displayable [HCons].
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct StdDisplayWrapper<T: ?Sized>(T);
-
-impl<T> fmt::Display for StdDisplayWrapper<T>
-where
-    T: ?Sized + MyDisplay,
-{
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.0.fmt(f)
-    }
-}
-
-impl<Head, Tail> MyDisplay for HCons<Head, Tail>
+/// let some_list = hlist!["abcde", 123, String::from("Wow!")];
+/// assert_eq!("abcde, 123, Wow!", some_list.to_string());
+/// ```
+impl<Head, Tail> fmt::Display for HeterogenousList<Head, Tail>
 where
     Head: fmt::Display,
-    Tail: MyDisplay,
+    Tail: fmt::Display,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}, {}", self.head, StdDisplayWrapper(&self.tail))
+        write!(f, "{}, {}", self.head, self.tail)
     }
 }
 
-impl<T> MyDisplay for HCons<T, HNil>
+impl<T> fmt::Display for HeterogenousList<T, ListTerminator>
 where
     T: fmt::Display,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         self.head.fmt(f)
     }
-}
-
-/// Wraps up the provided list into a type that implements [fmt::Display], when
-/// all types of the given list ([HNil] excluded) implement [fmt::Display].
-///
-/// The [`MyDisplay`] trait which is mentioned in the `where` clause is a helper
-/// trait which is implemented for all the types which implement [fmt::Display].
-///
-/// ```rust
-/// use playing_with_hlists::display::display_list;
-/// use frunk::hlist;
-///
-/// let some_list = hlist!["abcde", 123, String::from("Wow!")];
-/// assert_eq!("abcde, 123, Wow!", display_list(some_list).to_string());
-/// ```
-pub fn display_list<Head, Tail>(list: HCons<Head, Tail>) -> StdDisplayWrapper<HCons<Head, Tail>>
-where
-    Head: fmt::Display,
-    Tail: MyDisplay,
-{
-    StdDisplayWrapper(list)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,48 @@
-//! Experiments with heterogenous lists and [frunk].
+//! Experiments with heterogenous lists.
 //!
 //! Currently the following examples are implemented:
 //!
-//! * [display_list][display::display_list]: [Display][std::fmt::Display]
-//!   implementation for a heterogenous list.
+//! * [display]: [Display][std::fmt::Display] implementation for a
+//!   heterogenous list.
 //!
 //! [frunk]: https://github.com/lloydmeta/frunk
 
 #![deny(missing_docs)]
 #![deny(rustdoc::all)]
 
-/// Display heterogenous list.
+/// [Display][std::fmt::Display] implementation for heterogenous lists.
 pub mod display;
+
+/// A recursively-defined non-empty heterogenous list.
+///
+/// Use a [`hlist!`] macro to create an object of this type with multiple objects.
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct HeterogenousList<Head, Tail> {
+    /// First element of the list.
+    pub head: Head,
+
+    /// Rest elements of the list.
+    pub tail: Tail,
+}
+
+/// Terminating entry of a heterogenous list.
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct ListTerminator;
+
+/// Constructs a heterogenous list with at least one element.
+#[macro_export]
+macro_rules! hlist {
+    ( $head:expr $(,)? ) => {
+        $crate::HeterogenousList {
+            head: $head,
+            tail: $crate::ListTerminator,
+        }
+    };
+
+    ( $head:expr $(, $other:expr)+ $(,)? ) => {
+        $crate::HeterogenousList {
+            head: $head,
+            tail: $crate::hlist!( $($other),* ),
+        }
+    };
+}
